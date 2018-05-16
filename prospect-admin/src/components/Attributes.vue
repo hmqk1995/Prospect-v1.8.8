ce277b64398a8b4693c840d995660600205c7ed2
Kun Qian <qiankun1995@gmail.com>
2018-05-15 23:36:40 -0400

    attribute more style and whole style

--- prospect-admin/src/components/Attributes.vue
+++ prospect-admin/src/components/Attributes.vue
@@ -1,5 +1,39 @@
 <template>
   <div>
+    <div class="d-flex flex-wrap">
+      <div>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Add A New Attribute</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Add A New Attribute</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Add A New Attribute</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Add A New Attribute</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      </div>
+    </div>
     <table class="table table-striped table-bordered table-sm">
       <thead class="thead-dark">
         <tr>
@@ -44,4 +78,17 @@ export default {
 </script>
 
 <style scoped>
+.d-flex > div {
+  flex: 0 0 50%;
+  box-sizing: border-box;
+}
+.card {
+  margin: 10px;
+  background: #f1f8ff;
+  background: -moz-linear-gradient(top, #f1f8ff 0%, #d8ebff 100%);
+  background: -webkit-linear-gradient(top, #f1f8ff 0%,#d8ebff 100%);
+  background: linear-gradient(to bottom, #f1f8ff 0%,#d8ebff 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f1f8ff', endColorstr='#d8ebff',GradientType=0 );
+  border: none;
+}
 </style>
