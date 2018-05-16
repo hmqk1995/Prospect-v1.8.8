6646750fc1632ba75e339bdd306aa39279b22164
Kun Qian <qiankun1995@gmail.com>
2018-05-15 22:31:45 -0400

    table modify

--- prospect-admin/src/components/Attributes.vue
+++ prospect-admin/src/components/Attributes.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
-    <table class="table table-striped table-sm">
-      <thead>
+    <table class="table table-striped table-bordered table-sm">
+      <thead class="thead-dark">
         <tr>
-          <th width="5%" scope="col">#</th>
-          <th width="20%" scope="col">Attribute ID</th>
+          <th width="5%" scope="col"></th>
+          <th width="10%" scope="col">Attribute ID</th>
           <th width="20%" scope="col">Attribute Name</th>
-          <th width="55%" scope="col">Handle</th>
+          <th width="65%" scope="col">Operate</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(item, index) in info" :key=index>
-          <th scope="row">{{ index + 1 }}</th>
+          <th scope="row"><input type="checkbox"></th>
           <td>{{ item.id }}</td>
           <td>{{ item.post_name }}</td>
-          <td>@mdo</td>
+          <td></td>
         </tr>
       </tbody>
     </table>
