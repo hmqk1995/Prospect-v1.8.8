e6beaeed6f7d51528d67218cf7e78a3bccf4ce05
Kun Qian <qiankun1995@gmail.com>
2018-05-29 14:44:59 -0400

    color picker attached

--- prospect-admin/src/components/AttributeLegendPanel.vue
+++ prospect-admin/src/components/AttributeLegendPanel.vue
@@ -1,14 +1,28 @@
 <template>
   <div>
-    legend panel {{ legend }}
+    <h4>Attribute Legends</h4>
+    {{ legend }}
+    <div>
+      <div v-for="item in legend">
+        <div class="legend">{{ item.l }}</div>
+        <color-picker :color="item.v" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Colorpicker from './Colorpicker'
 export default {
   props: ['legend'],
+  components: {
+    'color-picker': Colorpicker
+  }
 }
 </script>
 
-<style>
+<style scoped>
+.legend {
+  display: inline-block;
+}
 </style>
---
+++ prospect-admin/src/components/Colorpicker.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="colorpicker">
+    <span class="colorlabel">{{ chosenColor }}</span>
+    <div class="colorbox" :style="{'background': chosenColor}"></div>
+    <chrome-picker :value="chosenColor" @input="updateValue" />
+  </div>
+</template>
+<script>
+import { Chrome } from 'vue-color'
+export default {
+  props: ['color'],
+  components: {
+    'chrome-picker': Chrome
+  },
+  data () {
+    return {
+      chosenColor: this.color,
+      toggle: false,
+    }
+  },
+  methods: {
+    updateValue(color) {
+      this.chosenColor = color.hex;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.colorpicker, .colorlabel, .colorbox {
+  display: inline-block;
+}
+.colorbox {
+  height: 30px;
+  width: 30px;
+  background: #111;
+}
+</style>
