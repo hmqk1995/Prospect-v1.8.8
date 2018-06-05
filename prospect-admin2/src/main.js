43219f1e7f057e7e970639e9eb94031be630ef8a
Kun Qian <qiankun1995@gmail.com>
2018-06-05 15:55:36 -0400

    load css first

--- prospect-admin2/src/main.js
+++ prospect-admin2/src/main.js
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -5,9 +7,6 @@ import store from './store'
 import BootstrapVue from 'bootstrap-vue'
 
 Vue.use(BootstrapVue);
-
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
